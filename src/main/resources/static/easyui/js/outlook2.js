4ca9edc9fbe81f299851348ebcd036491ba4ae44
NKMYHinata <xiriy@outlook.com>
2023-12-08 17:55:12 +0800

    war-let_bug修复

--- src/main/resources/static/easyui/js/outlook2.js
+++ src/main/resources/static/easyui/js/outlook2.js
@@ -2,8 +2,7 @@
 jQuery文档就绪事件处理器
 它确保在HTML文档加载和解析完成后再执行其中的脚本
 */
-$(function() {
-	// 页面加载完毕时初始化左侧菜单，绑定选项卡关闭事件
+$(function(){
 	InitLeftMenu();
 	tabClose();
 	tabCloseEven();
@@ -15,14 +14,14 @@ $(function() {
 用于初始化页面左侧菜单
 */
 function InitLeftMenu() {
-	$("#nav").accordion({ animate: false });
+	$("#nav").accordion({animate:false});
 
 	// 遍历菜单数据，动态创建菜单项
 	$.each(_menus.menus, function(i, n) {
-		let menulist = '';
-		menulist += '<ul>';
+		var menulist ='';
+		menulist +='<ul>';
 		$.each(n.menus, function(j, o) {
-			menulist += '<li><div><a href="' + o.menuid + '" href="#" rel="' + o.url + '" ><span class="icon ' + o.icon + '" >&nbsp;</span><span class="nav">' + o.menuname + '</span></a></div></li> ';
+			menulist += '<li><div><a ref="'+o.menuid+'" href="#" rel="' + o.url + '" ><span class="icon '+o.icon+'" >&nbsp;</span><span class="nav">' + o.menuname + '</span></a></div></li> ';
 		})
 		menulist += '</ul>';
 
@@ -35,24 +34,25 @@ function InitLeftMenu() {
 	});
 
 	// 绑定菜单项的点击事件，用于打开新的选项卡
-	$('.easyui-accordion li a').click(function() {
-		const tabTitle = $(this).children('.nav').text();
-		const url = $(this).attr("rel");
-		const menuid = $(this).attr("ref");
-		const icon = getIcon(menuid, icon);
+	$('.easyui-accordion li a').click(function(){
+		var tabTitle = $(this).children('.nav').text();
 
-		addTab(tabTitle, url, icon);
+		var url = $(this).attr("rel");
+		var menuid = $(this).attr("ref");
+		var icon = getIcon(menuid,icon);
+
+		addTab(tabTitle,url,icon);
 		$('.easyui-accordion li div').removeClass("selected");
 		$(this).parent().addClass("selected");
-	}).hover(function() {
+	}).hover(function(){
 		$(this).parent().addClass("hover");
-	}, function() {
+	},function(){
 		$(this).parent().removeClass("hover");
 	});
 
 	// 选中第一个菜单项
-	const panels = $('#nav').accordion('panels');
-	const t = panels[0].panel('options').title;
+	var panels = $('#nav').accordion('panels');
+	var t = panels[0].panel('options').title;
 	$('#nav').accordion('select', t);
 }
 
@@ -60,15 +60,16 @@ function InitLeftMenu() {
 获取左侧导航的图标
 根据提供的menuid从菜单数据中查找对应的图标类名
 */
-function getIcon(menuid) {
-	let icon = 'icon ';
+function getIcon(menuid){
+	var icon = 'icon ';
 	$.each(_menus.menus, function(i, n) {
 		$.each(n.menus, function(j, o) {
-			if (o.menuid == menuid) {
+			if(o.menuid==menuid){
 				icon += o.icon;
 			}
 		})
 	})
+
 	return icon;
 }
 
@@ -77,16 +78,16 @@ function getIcon(menuid) {
 用于在主区域中添加新的选项卡
 如果选项卡已存在，则选择该选项卡。否则创建一个新选项卡
 */
-function addTab(subtitle, url, icon) {
-	if (!$('#tabs').tabs('exists', subtitle)) {
-		$('#tabs').tabs('add', {
-			title: subtitle,
-			content: createFrame(url),
-			closable: true,
-			icon: icon
+function addTab(subtitle,url,icon){
+	if(!$('#tabs').tabs('exists',subtitle)){
+		$('#tabs').tabs('add',{
+			title:subtitle,
+			content:createFrame(url),
+			closable:true,
+			icon:icon
 		});
-	} else {
-		$('#tabs').tabs('select', subtitle);
+	}else{
+		$('#tabs').tabs('select',subtitle);
 		$('#mm-tabupdate').click();
 	}
 	tabClose();
@@ -96,31 +97,35 @@ function addTab(subtitle, url, icon) {
 创建iframe来显示页面内容
 用于在选项卡内显示指定的URL内容
 */
-function createFrame(url) {
-	return '<iframe scrolling="auto" frameborder="0"  src="' + url + '" style="width:100%;height:100%;"></iframe>';
+function createFrame(url)
+{
+	var s = '<iframe scrolling="auto" frameborder="0"  src="'+url+'" style="width:100%;height:100%;"></iframe>';
+	return s;
 }
 
 /*
 绑定选项卡的关闭事件
 包括双击关闭和右键菜单关闭
 */
-function tabClose() {
+function tabClose()
+{
 	// 双击关闭选项卡
-	$(".tabs-inner").dblclick(function() {
-		const subtitle = $(this).children(".tabs-closable").text();
-		$('#tabs').tabs('close', subtitle);
+	$(".tabs-inner").dblclick(function(){
+		var subtitle = $(this).children(".tabs-closable").text();
+		$('#tabs').tabs('close',subtitle);
 	})
 
 	// 绑定右键菜单
-	$(".tabs-inner").bind('contextmenu', function(e) {
+	$(".tabs-inner").bind('contextmenu',function(e){
 		$('#mm').menu('show', {
 			left: e.pageX,
 			top: e.pageY
 		});
 
-		const subtitle = $(this).children(".tabs-closable").text();
-		$('#mm').data("currtab", subtitle);
-		$('#tabs').tabs('select', subtitle);
+		var subtitle =$(this).children(".tabs-closable").text();
+
+		$('#mm').data("currtab",subtitle);
+		$('#tabs').tabs('select',subtitle);
 		return false;
 	});
 }
@@ -131,67 +136,66 @@ function tabClose() {
 */
 function tabCloseEven() {
 	// 刷新选项卡
-	$('#mm-tabupdate').click(function() {
-		const currTab = $('#tabs').tabs('getSelected');
-		const url = $(currTab.panel('options').content).attr('src');
-		$('#tabs').tabs('update', {
-			tab: currTab,
-			options: {
-				content: createFrame(url)
+	$('#mm-tabupdate').click(function(){
+		var currTab = $('#tabs').tabs('getSelected');
+		var url = $(currTab.panel('options').content).attr('src');
+		$('#tabs').tabs('update',{
+			tab:currTab,
+			options:{
+				content:createFrame(url)
 			}
 		})
 	})
 
 	// 关闭当前选项卡
-	$('#mm-tabclose').click(function() {
-		const currtab_title = $('#mm').data("currtab");
-		$('#tabs').tabs('close', currtab_title);
+	$('#mm-tabclose').click(function(){
+		var currtab_title = $('#mm').data("currtab");
+		$('#tabs').tabs('close',currtab_title);
 	})
 
 	// 关闭所有选项卡
-	$('#mm-tabcloseall').click(function() {
-		$('.tabs-inner span').each(function(i, n) {
-			const t = $(n).text();
-			$('#tabs').tabs('close', t);
+	$('#mm-tabcloseall').click(function(){
+		$('.tabs-inner span').each(function(i,n){
+			var t = $(n).text();
+			$('#tabs').tabs('close',t);
 		});
 	});
 
 	// 关闭除当前之外的选项卡
-	$('#mm-tabcloseother').click(function() {
+	$('#mm-tabcloseother').click(function(){
 		$('#mm-tabcloseright').click();
 		$('#mm-tabcloseleft').click();
 	});
-
-	// 关闭当前右侧的选项卡
-	$('#mm-tabcloseright').click(function() {
-		const nextall = $('.tabs-selected').nextAll();
-		if (nextall.length == 0) {
+	//关闭当前右侧的TAB
+	$('#mm-tabcloseright').click(function(){
+		var nextall = $('.tabs-selected').nextAll();
+		if(nextall.length==0){
+			//msgShow('系统提示','后边没有啦~~','error');
 			alert('后边没有啦~~');
 			return false;
 		}
-		nextall.each(function(i, n) {
-			const t = $('a:eq(0) span', $(n)).text();
-			$('#tabs').tabs('close', t);
+		nextall.each(function(i,n){
+			var t=$('a:eq(0) span',$(n)).text();
+			$('#tabs').tabs('close',t);
 		});
 		return false;
 	});
-
-	// 关闭当前左侧的选项卡
-	$('#mm-tabcloseleft').click(function() {
-		const prevall = $('.tabs-selected').prevAll();
-		if (prevall.length == 0) {
+	//关闭当前左侧的TAB
+	$('#mm-tabcloseleft').click(function(){
+		var prevall = $('.tabs-selected').prevAll();
+		if(prevall.length==0){
 			alert('到头了，前边没有啦~~');
 			return false;
 		}
-		prevall.each(function(i, n) {
-			const t = $('a:eq(0) span', $(n)).text();
-			$('#tabs').tabs('close', t);
+		prevall.each(function(i,n){
+			var t=$('a:eq(0) span',$(n)).text();
+			$('#tabs').tabs('close',t);
 		});
 		return false;
 	});
 
-	// 退出菜单
-	$("#mm-exit").click(function() {
+	//退出
+	$("#mm-exit").click(function(){
 		$('#mm').menu('hide');
 	})
 }
--- src/main/resources/static/easyui/js/validateExtends.js
+++ src/main/resources/static/easyui/js/validateExtends.js
@@ -13,15 +13,15 @@ $.extend($.fn.validatebox.defaults.rules, {
 	// 移动手机号码验证
 	mobile: { // value值为文本框中的值
 		validator: function(value) {
-			let reg = /^1[3|4|5|8|9]\d{9}$/;
+			var reg = /^1[3|4|5|8|9]\d{9}$/;
 			return reg.test(value);
 		},
-		message: '请输入有效的手机号码'
+		message: '13/14/15/18/19开头,且11位的手机号'
 	},
 	// 只能为数字
 	number: { // value值为文本框中的值
 		validator: function(value) {
-			let reg = /^[0-9]*$/;
+			var reg = /^[0-9]*$/;
 			return reg.test(value);
 		},
 		message: '只能输入数字'
@@ -29,14 +29,14 @@ $.extend($.fn.validatebox.defaults.rules, {
 	// 验证账号是否重复
 	repeat: {
 		validator: function(value) {
-			let flag = true;
+			var flag = true;
 			$.ajax({
 				type: "post",
 				async: false,
 				url: "SystemServlet?method=AllAccount&t=" + new Date().getTime(),
 				success: function(data) { // 异步请求检查账号是否重复
-					let account = $.parseJSON(data);
-					for (let i = 0; i < account.length; i++) {
+					var account = $.parseJSON(data);
+					for (var i = 0; i < account.length; i++) {
 						if (value == account[i]) {
 							flag = false;
 							break;
@@ -51,14 +51,14 @@ $.extend($.fn.validatebox.defaults.rules, {
 	// 验证课程是否重复
 	repeat_course: {
 		validator: function(value) {
-			let flag = true;
+			var flag = true;
 			$.ajax({
 				type: "post",
 				async: false,
 				url: "CourseServlet?method=CourseList&t=" + new Date().getTime(),
 				success: function(data) { // 异步请求检查课程是否重复
-					let course = $.parseJSON(data);
-					for (let i = 0; i < course.length; i++) {
+					var course = $.parseJSON(data);
+					for (var i = 0; i < course.length; i++) {
 						if (value == course[i].name) {
 							flag = false;
 							break;
@@ -73,14 +73,14 @@ $.extend($.fn.validatebox.defaults.rules, {
 	// 验证年级是否重复
 	repeat_grade: {
 		validator: function(value) {
-			let flag = true;
+			var flag = true;
 			$.ajax({
 				type: "post",
 				async: false,
 				url: "GradeServlet?method=GradeList&t=" + new Date().getTime(),
 				success: function(data) { // 异步请求检查年级是否重复
-					let grade = $.parseJSON(data);
-					for (let i = 0; i < grade.length; i++) {
+					var grade = $.parseJSON(data);
+					for (var i = 0; i < grade.length; i++) {
 						if (value == grade[i].name) {
 							flag = false;
 							break;
@@ -95,16 +95,16 @@ $.extend($.fn.validatebox.defaults.rules, {
 	// 验证班级是否重复
 	repeat_clazz: {
 		validator: function(value, param) {
-			let gradeid = $(param[0]).combobox("getValue");
-			let flag = true;
+			var gradeid = $(param[0]).combobox("getValue");
+			var flag = true;
 			$.ajax({
 				type: "post",
 				async: false,
 				data: { gradeid: gradeid },
 				url: "ClazzServlet?method=ClazzList&t=" + new Date().getTime(),
 				success: function(data) { // 异步请求检查班级是否重复
-					let clazz = $.parseJSON(data);
-					for (let i = 0; i < clazz.length; i++) {
+					var clazz = $.parseJSON(data);
+					for (var i = 0; i < clazz.length; i++) {
 						if (value == clazz[i].name) {
 							flag = false;
 							break;
@@ -116,7 +116,7 @@ $.extend($.fn.validatebox.defaults.rules, {
 		},
 		message: '该年级下已存在同名班级'
 	},
-	// 验证两个值是否相同（例如密码和确认密码）
+	// 验证两个值是否相同（密码和密码）
 	equals: {
 		validator: function(value, param) {
 			return $(param[0]).val() === value;
@@ -126,7 +126,7 @@ $.extend($.fn.validatebox.defaults.rules, {
 	// 密码规则验证
 	password: {
 		validator: function(value) {
-			let reg = /^[a-zA-Z0-9]{6,16}$/;
+			var reg = /^[a-zA-Z0-9]{6,16}$/;
 			return reg.test(value);
 		},
 		message: '密码长度为6-16位，只能包含英文和数字'
@@ -141,7 +141,7 @@ $.extend($.fn.validatebox.defaults.rules, {
 	// 国内邮编验证
 	zipcode: {
 		validator: function(value) {
-			let reg = /^[1-9]\d{5}$/;
+			var reg = /^[1-9]\d{5}$/;
 			return reg.test(value);
 		},
 		message: '请输入有效的邮编'
